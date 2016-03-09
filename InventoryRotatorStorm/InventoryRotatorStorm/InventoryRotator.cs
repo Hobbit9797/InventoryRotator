33fac9a8e44f51a58f1f85bd0cde5c3d01922cfb
Hobbit9797 <niklas.schulze1@gmx.net>
2016-03-09 17:42:28 +0100

    Added files via upload

---
+++ InventoryRotatorStorm/InventoryRotatorStorm/InventoryRotator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Storm.ExternalEvent;
+using Storm.StardewValley;
+using Storm.StardewValley.Event;
+using Storm.StardewValley.Wrapper;
+using Microsoft.Xna.Framework.Input;
+
+namespace InventoryRotator
+{
+    [Mod]
+    public class InventoryRotator : DiskResource
+    {
+        public Config ModConfig { get; private set; }
+        public KeyboardState lastState;
+        public GamePadState lastGPState;
+        public Keys keyboardKey;
+
+        [Subscribe]
+        public void InitializeCallback(InitializeEvent @event)
+        {
+            Console.WriteLine("Initializie InventoryRotator");
+            var configLocation = Path.Combine(PathOnDisk, "Config.json");
+            if (!File.Exists(configLocation))
+            {
+                Console.WriteLine("Create config file for InventoryRotator...");
+                ModConfig = new Config();
+                ModConfig.KeyboardKey = "Tab";
+                File.WriteAllBytes(configLocation, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(ModConfig)));
+                Console.WriteLine("Config file for InventoryRotator has been loaded.",
+                ModConfig.KeyboardKey, ModConfig.GamepadButton);
+            }
+            else
+            {
+                ModConfig = JsonConvert.DeserializeObject<Config>(Encoding.UTF8.GetString(File.ReadAllBytes(configLocation)));
+                Console.WriteLine("Config file for InventoryRotator has been loaded.",
+                    ModConfig.KeyboardKey);
+            }
+
+            keyboardKey = (Keys)Enum.Parse(typeof(Keys), ModConfig.KeyboardKey);
+            Console.WriteLine("Initializatiohn for InventoryRotator complete");
+        }
+
+
+        [Subscribe]
+        public void UpdateCallback(PreUpdateEvent @event)
+        {
+            //Console.WriteLine("Update");
+            var player = @event.LocalPlayer;
+            KeyboardState keyboard = Keyboard.GetState();
+            GamePadState gamepad = GamePad.GetState(Microsoft.Xna.Framework.PlayerIndex.One);
+            Keys[] pressedKeys = keyboard.GetPressedKeys();
+            //Buttons[] pressedButtons = gamepad.Buttons.
+            if (!player.Name.Equals(""))
+            {
+                //Console.WriteLine(player.Name+"Can move");
+                if ((keyboard.IsKeyDown(keyboardKey) && lastState.IsKeyUp(keyboardKey))||
+                    (gamepad.Buttons.LeftShoulder == ButtonState.Pressed && lastGPState.Buttons.LeftShoulder == ButtonState.Released)||
+                    (gamepad.Buttons.RightShoulder == ButtonState.Pressed && lastGPState.Buttons.RightShoulder == ButtonState.Released))
+                {   
+                    Console.WriteLine(ModConfig.KeyboardKey+" pressed");
+                    int invSize = player.Items.Count;
+                    Console.WriteLine("invSize:" + invSize);
+                    List<Item> newInventory = new List<Item>();
+                    for (int i = 12; i < invSize; i++)
+                    {
+                        if (player.Items[i] != null)
+                        {
+                            newInventory.Add(player.Items[i]);
+                            //Console.WriteLine("Added Item");
+                        }
+                        else
+                        {
+                            newInventory.Add(null);
+                            //Console.WriteLine("Added null");
+                        }
+                    }
+                    for (int i = 0; i < 12; i++)
+                    {
+                        if (player.Items[i] != null)
+                        {
+                            newInventory.Add(player.Items[i]);
+                            //Console.WriteLine("Added Item");
+                        }
+                        else
+                        {
+                            newInventory.Add(null);
+                            //Console.WriteLine("Added null");
+                        }
+                    }
+                    for (int i = 0; i < invSize; i++)
+                    {
+                        player.SetItem(i, null);
+                        if (newInventory[i] != null)
+                        {
+                            //Console.WriteLine(newInventory[i].Name + newInventory[i].Stack + " " + i);
+                            player.SetItem(i, newInventory[i]);
+                        }
+                        else
+                        {
+                            //Console.WriteLine(i + " is null");
+
+                        }
+                    }
+                }
+            }
+
+            lastState = keyboard;
+            lastGPState = gamepad;
+        }
+    }
+
+    public class Config
+    {
+        public String KeyboardKey { get; set; }
+        public String GamepadButton { get; set; }
+    }
+}
